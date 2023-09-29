61ccd2e81eee88e6c75775b363fcfa6fce933e3d
dev <dev@alxuser>
2023-09-29 10:51:30 +0530

    c

---
+++ src/utils/Constants.java
@@ -0,0 +1,10 @@
+package utils;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Constants {
+	public  static String SystemIp = "";
+	 public static long gapDuration =0;
+	 public static int DateWiseId = 0;
+	 public static ConcurrentLinkedQueue<String > consolelog = new ConcurrentLinkedQueue<String >();
+}
